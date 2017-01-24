3b6fdc269253b7b984ff7e79bde27f752d58a7f4
stetsiv87 <stetsiv87@gmail.com>
2017-01-24 13:11:16 +0200

    adding test row

--- src/test/java/Orlando/BaseTest.java
+++ src/test/java/Orlando/BaseTest.java
@@ -45,6 +45,7 @@ public class BaseTest extends NavigationHelper implements ProjectConfig,SQLHelpe
             ConfigDB.getPropsFromFile();
             SQLHelper.getDatasource();
             login(ConfigDB.getUrl(), ConfigDB.getTenant(), ConfigDB.getUserName(), ConfigDB.getTenantPassword());
+            System.out.println("Test row.....")
            
         }
     }
